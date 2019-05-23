4e84bdf196c99fc402b762bf79cc14c68a8f018b
Prathompong Prasert <paul.prasert@pcmicorp.com>
2019-05-23 19:26:36 +0700

    add firebase and readme

---
+++ README.md
@@ -0,0 +1,56 @@
+# MQTT API 101
+
+### Prerequisites
+* Node
+
+### Packages
+* mqtt
+* express
+* body-parser
+* firebase
+* firebase-admin
+* dotenv
+
+### Installing
+
+* Clone project to your local
+
+```
+git clone https://github.com/ppraserts/mqtt-api-101.git
+```
+
+* Go to directory
+
+```
+cd mqtt-api-101
+```
+* Install Package
+
+```
+Create ".env" file
+```
+* NODE_ENV=xxx
+* PORT=xxx
+* MQTT_HOST=xxx
+* MQTT_PORT=xxx
+* MQTT_USERNAME=xxx
+* MQTT_PASSWORD=xxx
+* FIREBASE_APIKEY=xxx
+* FIREBASE_AUTH_DOMAIN=xxx
+* FIREBASE_DATABASE_URL=xxx
+* FIREBASE_PROJECT_ID=xxx
+* FIREBASE_STORAGE_BUCKET=xxx
+* FIREBASE_MESSAGING_SENDER_ID=xxx
+* FIREBASE_APP_ID=xxx
+
+```
+npm install
+```
+* Start project
+
+```
+node app.js
+```
+## Running REST API
+
+Open browser: http://localhost:3000
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+var firebase = require('./FirebaseConfig');
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
@@ -10,11 +11,57 @@ app.use(bodyParser.urlencoded({ extended: true }))
 var mqttClient = new mqttHandler();
 mqttClient.connect();
 
-app.post("/ligth", function(req, res) {
-  mqttClient.sendMessage(req.body.message);
-  res.status(200).send("Message sent to mqtt");
+app.get('/ligth', function (req, res) {
+    console.log("HTTP Get Request");
+	var ligthReference = firebase.database().ref("/Ligths/");
+	ligthReference.on("value", function(snapshot) {
+        console.log(snapshot.val());
+        res.json(snapshot.val());
+        ligthReference.off("value");
+	}, 
+	function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+        res.send("The read failed: " + errorObject.code);
+	});
 });
 
-var server = app.listen(3000, function () {
+app.post('/ligth', function (req, res) {
+
+    if(req.body.User === undefined || req.body.User === "") {
+        return res.status(400).send({ message: 'User is required.' });
+    } 
+    else if(req.body.Name === undefined || req.body.Name === "") {
+        return res.status(400).send({ message: 'Name is required.' });
+    }
+    else if(req.body.Status === undefined || req.body.Status === "") {
+        return res.status(400).send({ message: 'Status is required.' });
+    }
+    else if(req.body.Status !== "on" && req.body.Status !== "off") {
+        return res.status(400).send({ message: 'Status must be \'on\' or \'off\'.' });
+    }
+
+    console.log("HTTP Post Request");
+    var user = req.body.User;
+	var name = req.body.Name;
+    var message = req.body.Message;
+    var status = req.body.Status;
+
+    // Publsh Message to MQTT
+    mqttClient.sendMessage(status);
+
+    // Save Data to Firebase
+	var referencePath = '/Ligths/'+user+'/';
+	var ligthReference = firebase.database().ref(referencePath);
+	ligthReference.set({Name: name, Message: message, Status: status}, function(error) {
+        if (error) {
+            res.status(400).send({ message: 'Data could not be saved.' + error });
+        } 
+        else {
+            res.status(200).json({ message: 'Data saved successfully.' });
+        }
+    });
+});
+
+var server = app.listen(process.env.PORT, function () {
     console.log("app running on port.", server.address().port);
 });
